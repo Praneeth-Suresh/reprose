050811c9610c6350a61608f058ad9fbf49ff7bbc
Praneeth Suresh <praneeth.suresh.s@gmail.com>
2023-09-10 11:03:37 +0800

    Sunday update

--- book_library/bookapp/bookapis.py
+++ book_library/bookapp/bookapis.py
@@ -1,11 +1,11 @@
 # Get the ISBN for the book
 
+import requests
 import os
 key = os.environ.get('Google_Cloud_API_Key')
 
 key = 'AIzaSyDxM0hcZbiTkjMmR6-japeENTi5418T6Wk'
 
-import requests
 
 def get_isbn_from_title(title, api_key):
     base_url = "https://www.googleapis.com/books/v1/volumes"
@@ -29,8 +29,10 @@ def get_isbn_from_title(title, api_key):
         return "ISBN not found"
     except Exception as e:
         return f"An error occurred: {str(e)}"
-    
-def get_book_info_from_isbn(isbn, api_key):
+
+
+def get_book_info_from_isbn(isbn):
+    api_key = 'AIzaSyDxM0hcZbiTkjMmR6-japeENTi5418T6Wk'
     base_url = "https://www.googleapis.com/books/v1/volumes"
     params = {
         "q": f"isbn:{isbn}",
@@ -45,22 +47,26 @@ def get_book_info_from_isbn(isbn, api_key):
             book = data["items"][0]
             volume_info = book.get("volumeInfo", {})
             title = volume_info.get("title", "Title not found")
-            subtitle = volume_info.get("subtitle", "Subtitle not found")
-            image_links = volume_info.get("imageLinks", {})
-            image_url = image_links.get("thumbnail", "Image not found")
+            description = volume_info.get(
+                "description", "Description not found")
+            categories = volume_info.get("categories", "Categories not found")
+            averageRating = volume_info.get(
+                "averageRating", "Rating not found")
+            maturityRating = volume_info.get(
+                "maturityRating", "Maturity rating not found")
+            image_url = "https://covers.openlibrary.org/b/isbn/" + isbn + "-L.jpg"
 
-            return title, subtitle, image_url
+            return title, description, categories, averageRating, maturityRating, image_url
 
         return "Book not found"
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-if __name__ == "__main__":
-    api_key = key  
-    book_title = input("Enter the book title: ")
-    isbn = get_isbn_from_title(book_title, api_key)
-    print(f"ISBN for '{book_title}': {isbn}")
 
-def get_data_from_isbn(isbn, api_key):
-    
-    return(isbn.data())
+# print(get_book_info_from_isbn(9781593156329, key))
+
+if __name__ == "__main__":
+    api_key = key
+    book_isbn = input("Enter the book isbn: ")
+    book_info = get_book_info_from_isbn(book_isbn)
+    print(book_info)
